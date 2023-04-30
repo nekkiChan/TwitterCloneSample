42a3ec21960c397777f716316fe5719c74470dd8
nekkiChan <nekkiChan@users.noreply.github.com>
2023-04-30 17:07:20 +0900

    会員登録画面&ログイン画面

---
+++ Views/sign-in.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="../Views/img/logo-twitterblue.svg">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link rel="stylesheet" href="../Views/css/style.css">
+
+    <title>ログイン画面 / Twitterクローン</title>
+    <meta name="description" content="ログイン画面です">
+</head>
+
+<body class="signup text-center">
+    <main class="form-signup">
+        <form action="sign-in.php" method="post">
+            <img src="../Views/img/logo-white.svg" alt="" class="logo-white">
+            <h1>Twitterクローンにログイン</h1>
+            <input type="email" class="form-control" name="email" placeholder="メールアドレス" required autofocus>
+            <input type="password" class="form-control" name="password" placeholder="パスワード" required>
+            <button class="w-100 btn btn-lg" type="submit">ログインする</button>
+            <p class="mt-3 mb-2"><a href="sign-up.php">会員登録する</a></p>
+            <p class="mt-2 mb-3 text-muted">&copy; 2023</p>
+        </form>
+    </main>
+</body>
+
+</html>
---
+++ Views/sign-up.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="../Views/img/logo-twitterblue.svg">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link rel="stylesheet" href="../Views/css/style.css">
+
+    <title>会員登録画面 / Twitterクローン</title>
+    <meta name="description" content="会員登録画面です">
+</head>
+
+<!--
+    Bootstrapのクラス
+    text-center: 'text-align:center;'と同じ。
+    w-100: 'width:100%;'と同じ。
+    btn-lg: 大きなボタン。
+    mt-3: 'margin-top:1rem;'と同じ。
+    mb-2: 'margin-bottom:0.5rem;'と同じ。
+    text-muted: 文字を灰色にする。
+-->
+
+<body class="signup text-center">
+    <main class="form-signup">
+        <form action="sign-up.php" method="post">
+            <img src="../Views/img/logo-white.svg" alt="" class="logo-white">
+            <h1>アカウントを作る</h1>
+            <input type="text" class="form-control" name="nickname" placeholder="ニックネーム" maxlength="50" required
+                autofocus>
+            <input type="text" class="form-control" name="name" placeholder="ユーザー名、例）techiis02" maxlength="50" required>
+            <input type="email" class="form-control" name="email" placeholder="メールアドレス" maxlength="254" required>
+            <input type="password" class="form-control" name="password" placeholder="パスワード" minlength="4" maxlength="50"
+                required>
+            <button class="w-100 btn btn-lg" type="submit">登録する</button>
+            <p class="mt-3 mb-2"><a href="sign-in.php">ログインする</a></p>
+            <p class="mt-2 mb-3 text-muted">&copy; 2023</p>
+        </form>
+    </main>
+</body>
+
+</html>
