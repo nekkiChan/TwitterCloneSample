a3de0955e26d8d4f90767f71316bb0ef65d697fe
nekkiChan <nekkiChan@users.noreply.github.com>
2023-05-05 16:58:08 +0900

    ログイン機能：コントローラーを作成

--- Controllers/sign-in.php
+++ Controllers/sign-in.php
@@ -1,6 +1,38 @@
 <?php
 // OVERVIEW:サインインコントローラー
 
+// 設定を読み込み
+include_once '../config.php';
+// 便利な関数を読み込み
+include_once '../util.php';
+
+// ユーザーデータ操作モデルを読み込み
+include_once '../Models/users.php';
+
+/** ログイン結果 */
+$try_login_result = null;
+
+// メールアドレスとパスワードが入力されている場合
+if (isset($_POST['email']) && isset($_POST['password'])) {
+    // ログインチェック実行
+$user = [];
+    // ログインに成功した場合
+    if ($user) {
+        // ユーザー情報をセッションに保存
+        
+        // ホーム画面へ遷移
+        header('Location: ' . HOME_URL . 'Controllers/home.php');
+        exit;
+    } else {
+        // ログイン結果を失敗にする
+        $try_login_result = false;
+    }
+}
+
+/** ログイン結果（表示用） */
+$view_try_login_result = $try_login_result;
+
 //  画面表示
 include_once '../Views/sign-in.php';
+
 ?>
--- Views/sign-in.php
+++ Views/sign-in.php
@@ -1,12 +1,3 @@
-<?php
-
-// 設定関連を読み込む
-include_once('../config.php');
-// 便利な関数を読み込む
-include_once('../util.php');
-
-?>
-
 <!DOCTYPE html>
 <html lang="ja">
 
@@ -21,6 +12,13 @@ include_once('../util.php');
         <form action="sign-in.php" method="post">
             <img src="../Views/img/logo-white.svg" alt="" class="logo-white">
             <h1>Twitterクローンにログイン</h1>
+
+            <?php if (isset($view_try_login_result) && $view_try_login_result === false): ?>
+                <div class="alert alert-warning text-sm" role="alert">
+                    ログインに失敗しました。メールアドレス、パスワードが正しいかご確認ください。
+                </div>
+            <?php endif ?>
+
             <input type="email" class="form-control" name="email" placeholder="メールアドレス" required autofocus>
             <input type="password" class="form-control" name="password" placeholder="パスワード" required>
             <button class="w-100 btn btn-lg" type="submit">ログインする</button>
