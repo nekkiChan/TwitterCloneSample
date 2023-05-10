65dd046e94ab3e0ae4a1c674bb8553b2f909d8ae
nekkiChan <nekkiChan@users.noreply.github.com>
2023-05-10 17:46:57 +0900

    ログイン機能：ユーザーモデルにログイン関数を追加

--- Controllers/sign-in.php
+++ Controllers/sign-in.php
@@ -15,11 +15,12 @@ $try_login_result = null;
 // メールアドレスとパスワードが入力されている場合
 if (isset($_POST['email']) && isset($_POST['password'])) {
     // ログインチェック実行
-$user = [];
+    $user = findUserAndCheckPassword($_POST['email'], $_POST['password']);
+
     // ログインに成功した場合
     if ($user) {
         // ユーザー情報をセッションに保存
-        
+
         // ホーム画面へ遷移
         header('Location: ' . HOME_URL . 'Controllers/home.php');
         exit;
--- Models/users.php
+++ Models/users.php
@@ -2,6 +2,7 @@
 // OVERVIEW:ユーザーデータを処理
 
 /**
+ * ユーザーを作成
  * @param array $data
  * @return bool
  */
@@ -41,5 +42,60 @@ function createUser(array $data)
     $mysqli->close();
 
     return $response;
+}
+
+/**
+ * ユーザー情報取得：ログインチェック
+ * @param string $email
+ * @param string $password
+ * @return array|false
+ */
+function findUserAndCheckPassword(string $email, string $password)
+{
+    // DB接続
+    $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+
+    // 接続エラーがある場合->処理停止
+    if ($mysqli->connect_errno) {
+        echo 'MySQLの接続に失敗しました。：' . $mysqli->connect_errno . "\n";
+        exit;
+    }
+
+    // 入力値をエスケープ
+    $email = $mysqli->real_escape_string($email);
+
+    // SQLクエリを作成
+    // - 外部からのリクエストは何が入ってくるかわからないので、必ず、エスケープしたものをクオートで囲む。
+    $query = 'SELECT * FROM users WHERE email = "' . $email . '"';
+
+    // クエリ実行
+    $result = $mysqli->query($query);
+
+    // クエリ実行に失敗した場合->return
+    if (!$result) {
+        // MySQL処理中にエラー発生
+        echo 'エラーメッセージ：' . $mysqli->error . "\n";
+        $mysqli->close();
+        return false;
+    }
+
+    // ユーザー情報を取得
+    $user = $result->fetch_array(MYSQLI_ASSOC);
+    // ユーザーが存在しない場合->return
+    if (!$user) {
+        $mysqli->close();
+        return false;
+    }
+
+    // パスワードチェック、不一致の場合->return
+    if (!password_verify($password, $user['password'])) {
+        $mysqli->close();
+        return false;
+    }
+
+    // DB接続を解放
+    $mysqli->close();
+
+    return $user;
 }
 ?>
